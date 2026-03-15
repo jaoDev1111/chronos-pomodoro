d26b777c9807c9b9b1ebcd5901320b42a30bfe62
Jota the Dev <joaoguilhermeavelino387@gmail.com>
2026-03-15 14:00:35 -0300

    feat: implement TimerWorkerManager with web worker for countdown

--- src/contexts/TaskProvider/TaskProvider.tsx
+++ src/contexts/TaskProvider/TaskProvider.tsx
@@ -5,6 +5,7 @@ import { useEffect, useReducer } from 'react';
 import { TaskContext } from '../TaskContext/TaskContext';
 import { initialTaskState } from '../../states/initialTaskState';
 import { taskReducer } from '../../reducers/taskReducer';
+import { TimerWorkerManager } from '../../workers/TimerWorkerManager';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -13,9 +14,27 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
 
+  const worker = TimerWorkerManager.getInstance();
+
+  worker.onmessage(event => {
+    const countDownSeconds = event.data;
+
+    console.log(countDownSeconds);
+
+    if (event.data <= 0) {
+      console.log('Worker COMPLETED');
+      worker.terminate();
+    }
+  });
+
   useEffect(() => {
-    console.log(state);
-  }, [state]);
+    if (!state.activeTask) {
+      console.log('Worker terminado por falta de activeTask');
+      worker.terminate();
+    }
+
+    worker.postMessage(state);
+  }, [state, worker]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
